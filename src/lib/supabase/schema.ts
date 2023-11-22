9b533fc37be52019b96cc656011fdfacce1b926a
Lalit sharma <geekymonkcp742k@gmail.com>
2023-11-22 18:39:59 +0530

    Implement 'files' table linking to 'workspaces' and 'folders

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -37,3 +37,28 @@ export const folders = pgTable("folders", {
       onDelete: "cascade",
     }),
 });
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id, {
+      onDelete: "cascade",
+    }),
+});
