cfa6f460ce85be5f1533e58c459b295d8ec67302
Lalit sharma <geekymonkcp742k@gmail.com>
2023-12-11 15:34:57 +0530

    site layout

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      {/* <Header /> */}
+      {children}
+    </main>
+  );
+};
+
+export default HomePageLayout;
