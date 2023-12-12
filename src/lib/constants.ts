ede8b4f3460c0483e985aa6d15515c45818c3e86
Lalit sharma <geekymonkcp742k@gmail.com>
2023-12-12 09:47:21 +0530

    Define custom client avatar using shadcn, user testimonials, pricing details, and constants.

---
+++ src/lib/constants.ts
@@ -0,0 +1,142 @@
+import client1 from "../../public/client1.png";
+import client2 from "../../public/client2.png";
+import client3 from "../../public/client3.png";
+import client4 from "../../public/client4.png";
+import client5 from "../../public/client5.png";
+
+export const CLIENTS = [
+  { alt: "client1", logo: client1 },
+  { alt: "client2", logo: client2 },
+  { alt: "client3", logo: client3 },
+  { alt: "client4", logo: client4 },
+  { alt: "client5", logo: client5 },
+];
+
+export const USERS = [
+  {
+    name: "Aham",
+    message:
+      "Sphere has been a game-changer for our team. With its reliable end-to-end testing, we catch bugs early, leading to faster development cycles and improved collaboration.",
+  },
+  {
+    name: "Dhruv",
+    message:
+      "I used to spend hours debugging frontend issues, but Sphere simplified everything. Now, I'm more productive, and my colleagues can trust our code thanks to Sphere.",
+  },
+  {
+    name: "Abhishek",
+    message:
+      "Sphere has transformed the way we work. Our QA and development teams are on the same page, and our productivity has skyrocketed. It's a must-have tool.",
+  },
+  {
+    name: "Bhavya",
+    message:
+      "I was skeptical at first, but Sphere exceeded my expectations. Our project timelines have improved, and collaboration between teams is seamless.",
+  },
+  {
+    name: "Kriti",
+    message:
+      "Sphere made writing and running tests a breeze. Our team's productivity has never been higher, and we're delivering more reliable software.",
+  },
+  {
+    name: "Shubham",
+    message:
+      "Thanks to Sphere, we've eliminated testing bottlenecks. Our developers and testers collaborate effortlessly, resulting in quicker releases.",
+  },
+  {
+    name: "Siddharth",
+    message:
+      "Sphere has improved our development process significantly. We now have more time for innovation, and our products are of higher quality.",
+  },
+  {
+    name: "Sneha",
+    message:
+      "Sphere's user-friendly interface made it easy for our non-technical team members to contribute to testing. Our workflow is much more efficient now.",
+  },
+  {
+    name: "Soham",
+    message:
+      "Our team's collaboration improved immensely with Sphere. We catch issues early, leading to less friction and quicker feature deployments.",
+  },
+  {
+    name: "Sushant",
+    message:
+      "Sphere's robust testing capabilities have elevated our development standards. We work more harmoniously, and our releases are more reliable.",
+  },
+  {
+    name: "Tanya",
+    message:
+      "Sphere is a lifesaver for our cross-functional teams. We're more productive, and there's a shared sense of responsibility for product quality.",
+  },
+  {
+    name: "Vedant",
+    message:
+      "Sphere has helped us maintain high standards of quality. Our team's collaboration has improved, resulting in faster development cycles.",
+  },
+  {
+    name: "Vishal",
+    message:
+      "Sphere is a powerful tool that improved our productivity and collaboration. It's now an integral part of our development process.",
+  },
+  {
+    name: "Yash",
+    message:
+      "Sphere's user-friendly interface and detailed reporting have made testing a breeze. Our team's productivity is at an all-time high.",
+  },
+  {
+    name: "Zoya",
+    message:
+      "We saw immediate benefits in terms of productivity and collaboration after adopting Sphere. It's an essential tool for our development workflow.",
+  },
+  {
+    name: "Aarav",
+    message:
+      "Sphere has streamlined our testing process and brought our teams closer. We're more efficient and deliver better results.",
+  },
+  {
+    name: "Aarushi",
+    message:
+      "Sphere has been a game-changer for us. Our productivity and collaboration have improved significantly, leading to better software.",
+  },
+  {
+    name: "Aayush",
+    message:
+      "Thanks to Sphere, our testing process is now a seamless part of our development cycle. Our teams collaborate effortlessly.",
+  },
+  {
+    name: "Abeer",
+    message:
+      "Sphere is a fantastic tool that has revolutionized our workflow. Our productivity and collaboration have reached new heights.",
+  },
+];
+
+export const PRICING_CARDS = [
+  {
+    planType: "Free Plan",
+    price: "0",
+    description: "Limited block trials  for teams",
+    highlightFeature: "",
+    features: [
+      "Unlimited blocks for teams",
+      "Unlimited file uploads",
+      "30 day page history",
+      "Invite 2 guests",
+    ],
+  },
+  {
+    planType: "Pro Plan",
+    price: "12.99",
+    description: "Billed annually. $17 billed monthly",
+    highlightFeature: "Everything in free +",
+    features: [
+      "Unlimited blocks for teams",
+      "Unlimited file uploads",
+      "1 year day page history",
+      "Invite 10 guests",
+    ],
+  },
+];
+
+export const PRICING_PLANS = { proplan: "Pro Plan", freeplan: "Free Plan" };
+
+export const MAX_FOLDERS_FREE_PLAN = 3;
