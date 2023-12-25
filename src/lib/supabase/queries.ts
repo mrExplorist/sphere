120bf479f2a79020d99c1cd58ebcafe4ad7089b1
Lalit sharma <geekymonkcp742k@gmail.com>
2023-12-25 21:56:07 +0530

    feat(api): Add getFolders function to fetch workspace folders from the db

--- src/lib/supabase/queries.ts
+++ src/lib/supabase/queries.ts
@@ -1,9 +1,14 @@
-import { eq } from "drizzle-orm";
-import { files, workspaces } from "../../../migrations/schema";
-import db from "./db";
-import { Subscription, workspace } from "./supabase.types";
-import { validate } from "uuid";
+"use server";
 
+import { and, eq, notExists } from "drizzle-orm";
+import { files, folders, workspaces } from "../../../migrations/schema";
+import db from "./db";
+import { Folder, Subscription, workspace } from "./supabase.types";
+import { validate } from "uuid";
+import { collaborators } from "./schema";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Queries for communicating with the database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Query for creating a new workspace
 export const createWorkspace = async (workspace: workspace) => {
   try {
     const response = await db.insert(workspaces).values(workspace);
@@ -14,6 +19,7 @@ export const createWorkspace = async (workspace: workspace) => {
   }
 };
 
+// Query for getting user subscription status
 export const getUserSubscriptionStatus = async (userId: string) => {
   try {
     const data = await db.query.subscriptions.findFirst({
@@ -27,6 +33,7 @@ export const getUserSubscriptionStatus = async (userId: string) => {
   }
 };
 
+// Query for getting files
 export const getFiles = async (folderId: string) => {
   const isValid = validate(folderId);
   if (!isValid) return { data: null, error: "Error" };
@@ -42,3 +49,22 @@ export const getFiles = async (folderId: string) => {
     return { data: null, error: "Error" };
   }
 };
+
+// Query for getting folders
+
+export const getFolders = async (workspaceId: string) => {
+  const isValid = validate(workspaceId);
+  if (!isValid) return { data: null, error: "Error" };
+  try {
+    const results: Folder[] | [] = await db
+      .select()
+      .from(folders)
+      .orderBy(folders.createdAt)
+      .where(eq(folders.workspaceId, workspaceId));
+    return { data: results, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: "Error getting folders from db" };
+  }
+};
+
