a07318ffad32d8813602c676ee06417e49ecb369
Lalit sharma <geekymonkcp742k@gmail.com>
2023-12-28 08:41:14 +0530

    Add UI for creating a workspace, including form layout  and input validation handling.

--- src/components/dashboard-setup/dashboard-setup.tsx
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -107,7 +107,90 @@ const DashboardSetup: React.FC<DashboardSetupProps> = ({ subscription, user }) =
     }
   };
 
-  return <div></div>;
+  return (
+    <Card
+      className="w-[800px]
+      h-screen
+      sm:h-auto
+  "
+    >
+      <CardHeader>
+        <CardTitle>Create A Workspace</CardTitle>
+        <CardDescription>
+          Lets create a private workspace to get you started.You can add collaborators later from the workspace settings
+          tab.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-4">
+            <div
+              className="flex
+            items-center
+            gap-4"
+            >
+              <div className="w-full ">
+                <Label
+                  htmlFor="workspaceName"
+                  className="text-sm
+                  text-muted-foreground
+                "
+                >
+                  Name
+                </Label>
+                <Input
+                  id="workspaceName"
+                  type="text"
+                  placeholder="Workspace Name"
+                  disabled={isLoading}
+                  {...register('workspaceName', {
+                    required: 'Workspace name is required',
+                  })}
+                />
+                <small className="text-red-600">{errors?.workspaceName?.message?.toString()}</small>
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="logo"
+                className="text-sm
+                  text-muted-foreground
+                "
+              >
+                Workspace Logo
+              </Label>
+              <Input
+                id="logo"
+                type="file"
+                accept="image/*"
+                placeholder="Workspace Name"
+                // disabled={isLoading || subscription?.status !== 'active'}
+                {...register('logo', {
+                  required: false,
+                })}
+              />
+              <small className="text-red-600">{errors?.logo?.message?.toString()}</small>
+              {subscription?.status !== 'active' && (
+                <small
+                  className="
+                  text-muted-foreground
+                  block
+              "
+                >
+                  To customize your workspace, you need to be on a Pro Plan
+                </small>
+              )}
+            </div>
+            <div className="self-end">
+              <Button disabled={isLoading} type="submit">
+                {!isLoading ? 'Create Workspace' : <Loader />}
+              </Button>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DashboardSetup;
