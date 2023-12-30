4198eca32e661da8af6a0f964f742a20dbb1b3d4
Lalit sharma <geekymonkcp742k@gmail.com>
2023-12-31 01:40:17 +0530

    feat(db query): Implemented updateFile function using Supabase to update a file with partial data. Utilized Supabase query to set specified fields and conditions for the update operation.

--- src/lib/supabase/queries.ts
+++ src/lib/supabase/queries.ts
@@ -185,11 +185,22 @@ export const updateFolder = async (folder: Partial<Folder>, folderId: string) =>
   }
 };
 
-// Create New Folder
+// Create New File
 
 export const createFile = async (file: File) => {
   try {
     await db.insert(files).values(file);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
+
+// Update file
+export const updateFile = async (file: Partial<File>, fileId: string) => {
+  try {
+    const response = await db.update(files).set(file).where(eq(files.id, fileId));
     return { data: null, error: null };
   } catch (error) {
     console.log(error);
