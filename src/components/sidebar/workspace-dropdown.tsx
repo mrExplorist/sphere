8edd675503cea787bf813b1f34b571c6be70faa2
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-01 14:59:35 +0530

    refactor fix some issue on workspace settings change

--- src/components/sidebar/workspace-dropdown.tsx
+++ src/components/sidebar/workspace-dropdown.tsx
@@ -1,14 +1,11 @@
-// Import necessary dependencies and components
 'use client';
 import { useAppState } from '@/lib/providers/state-provider';
 import { workspace } from '@/lib/supabase/supabase.types';
 import React, { useEffect, useState } from 'react';
-
 import SelectedWorkspace from './selected-workspace';
 import CustomDialogTrigger from '../global/custom-dialog-trigger';
 import WorkspaceCreator from '../global/workspace-creator';
 
-// Define prop types for the WorkspaceDropdown component
 interface WorkspaceDropdownProps {
   privateWorkspaces: workspace[] | [];
   sharedWorkspaces: workspace[] | [];
@@ -16,21 +13,16 @@ interface WorkspaceDropdownProps {
   defaultValue: workspace | undefined;
 }
 
-// WorkspaceDropdown component
 const WorkspaceDropdown: React.FC<WorkspaceDropdownProps> = ({
   privateWorkspaces,
   collaboratingWorkspaces,
   sharedWorkspaces,
   defaultValue,
 }) => {
-  // Access application state using the useAppState hook
   const { dispatch, state } = useAppState();
-
-  // Local state to manage selected workspace and dropdown visibility
   const [selectedOption, setSelectedOption] = useState(defaultValue);
   const [isOpen, setIsOpen] = useState(false);
 
-  // Effect to initialize the state with workspaces if it's empty
   useEffect(() => {
     if (!state.workspaces.length) {
       dispatch({
@@ -45,77 +37,96 @@ const WorkspaceDropdown: React.FC<WorkspaceDropdownProps> = ({
     }
   }, [privateWorkspaces, collaboratingWorkspaces, sharedWorkspaces]);
 
-  // Event handler to update the selected workspace
   const handleSelect = (option: workspace) => {
     setSelectedOption(option);
     setIsOpen(false);
   };
 
-  // Effect to select a workspace based on the defaultValue
-  useEffect(() => {
-    const findSelectedWorkspace = state.workspaces.find((workspace) => workspace.id === defaultValue?.id);
-    if (findSelectedWorkspace) setSelectedOption(findSelectedWorkspace);
-  }, [state, defaultValue]);
-
   return (
-    <div className="relative inline-block text-left">
+    <div
+      className=" relative inline-block
+      text-left
+  "
+    >
       <div>
-        {/* Clickable area to toggle dropdown visibility */}
         <span onClick={() => setIsOpen(!isOpen)}>
           {selectedOption ? <SelectedWorkspace workspace={selectedOption} /> : 'Select a workspace'}
         </span>
       </div>
-
-      {/* Dropdown content */}
       {isOpen && (
-        <div className="origin-top-right absolute w-full rounded-md shadow-md z-50 h-[190px] bg-black/10 backdrop-blur-lg group border-[1px] border-muted">
-          <div className="rounded-md flex flex-col ">
+        <div
+          className="origin-top-right
+          absolute
+          w-full
+          rounded-md
+          shadow-md
+          z-50
+          h-[190px]
+          bg-black/10
+          backdrop-blur-lg
+          group
+          overflow-y-scroll
+          scrollbar-thumb-[#030012] scrollbar-track-[#030012]
+          scrollbar-thin
+          border-[1px]
+          border-muted
+      "
+        >
+          <div className="rounded-md flex flex-col">
             <div className="!p-2">
-              {/* Render private workspaces if available */}
               {!!privateWorkspaces.length && (
                 <>
                   <p className="text-muted-foreground">Private</p>
                   <hr></hr>
                   {privateWorkspaces.map((option) => (
-                    // Map and render private workspaces
                     <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
                   ))}
                 </>
               )}
-
-              {/* Render shared workspaces if available */}
               {!!sharedWorkspaces.length && (
                 <>
                   <p className="text-muted-foreground">Shared</p>
                   <hr />
                   {sharedWorkspaces.map((option) => (
-                    // Map and render shared workspaces
                     <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
                   ))}
                 </>
               )}
-
-              {/* Render collaborating workspaces if available */}
               {!!collaboratingWorkspaces.length && (
                 <>
                   <p className="text-muted-foreground">Collaborating</p>
                   <hr />
                   {collaboratingWorkspaces.map((option) => (
-                    // Map and render collaborating workspaces
                     <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
                   ))}
                 </>
               )}
             </div>
-
-            {/* Trigger to create a new workspace */}
             <CustomDialogTrigger
               header="Create A Workspace"
               content={<WorkspaceCreator />}
               description="Workspaces give you the power to collaborate with others. You can change your workspace privacy settings after creating the workspace too."
             >
-              <div className="flex transition-all hover:bg-muted justify-center items-center gap-2 p-2 w-full">
-                <article className="text-slate-500 rounded-full bg-slate-800 w-4 h-4 flex items-center justify-center">
+              <div
+                className="flex
+              transition-all
+              hover:bg-muted
+              justify-center
+              items-center
+              gap-2
+              p-2
+              w-full"
+              >
+                <article
+                  className="text-slate-500
+                rounded-full
+                 bg-slate-800
+                 w-4
+                 h-4
+                 flex
+                 items-center
+                 justify-center"
+                >
                   +
                 </article>
                 Create workspace
