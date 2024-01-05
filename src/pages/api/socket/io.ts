62f14cf1e1c813d9f378ec3b507e5ee7391e1501
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-05 17:17:33 +0530

    Initialize Socket.IO server, handle real-time events, log initialization, and store instance for seamless communication in Next.js API route.

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -10,38 +10,59 @@ export const config = {
   },
 };
 
-// io handler
+// 1. create-room
+// 2. send-changes
+// 3. send-cursor-move
+
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  // Check if the Socket.IO server has been initialized for the current HTTP server
   if (!res.socket.server.io) {
+    // If not initialized, set up the path for Socket.IO
     const path = '/api/socket/io';
     console.log(`* First use, starting socket.io on path "${path}"`);
 
+    // Get the underlying HTTP server from the response object
     const httpServer: NetServer = res.socket.server as any;
 
-    // create socket.io server
+    // Create a new Socket.IO server instance, passing the HTTP server and configuration options
     const io = new ServerIO(httpServer, {
       path,
       addTrailingSlash: false,
     });
 
-    // handle socket.io events
+    // Handle Socket.IO events when a client connects
+    // Listening to events
     io.on('connection', (s) => {
+      // Event handler for a new client connection
       console.log('a user connected');
+
+      // Event handler for 'create-room' event
       s.on('create-room', (fileId) => {
-        console.log('🔵 create-room ');
+        console.log('🔵 create-room');
+        // Join the specified room (identified by fileId)
         s.join(fileId);
       });
+
+      // Event handler for 'send-changes' event
       s.on('send-changes', (deltas, fileId) => {
         console.log('📤 send-changes');
+        // Broadcast received changes to all clients in the same room (fileId)
         s.to(fileId).emit('receive-changes', deltas, fileId);
       });
+
+      // Event handler for 'send-cursor-move' event
       s.on('send-cursor-move', (range, fileId, cursorId) => {
         console.log('📤 send-cursor-move');
+        // Broadcast received cursor movement to all clients in the same room (fileId)
         s.to(fileId).emit('receive-cursor-move', range, fileId, cursorId);
       });
     });
+
+    // Store the Socket.IO instance on the HTTP server for future reference
     res.socket.server.io = io;
   }
+
+  // End the response
   res.end();
 };
 
