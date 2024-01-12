f6f0ba06494c0fa5865f2779504fa05e4531217a
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-12 15:19:32 +0530

    feat(stripe/admin-tasks): Implement manageSubscriptionStatusChange function - Manages changes in subscription status by updating the Supabase database with relevant subscription details. Additionally, triggers billing details copying if specified and applicable. Handles potential errors gracefully.

--- src/lib/stripe/admin-tasks.ts
+++ src/lib/stripe/admin-tasks.ts
@@ -1,9 +1,10 @@
 import Stripe from 'stripe';
-import { Price, Product } from '../supabase/supabase.types';
+import { Price, Product, Subscription } from '../supabase/supabase.types';
 import db from '../supabase/db';
-import { customers, prices, products, users } from '../../../migrations/schema';
+import { customers, prices, products, subscriptions, users } from '../../../migrations/schema';
 import { stripe } from './index';
 import { eq } from 'drizzle-orm';
+import { toDateTime } from '../utils';
 export const upsertProductRecord = async (product: Stripe.Product) => {
   const productData: Product = {
     id: product.id,
@@ -88,5 +89,52 @@ export const copyBillingDetailsToCustomer = async (uuid: string, payment_method:
       .where(eq(users.id, uuid));
   } catch (error) {
     throw new Error('Couldnot copy customer billing details');
+  }
+};
+
+export const manageSubscriptionStatusChange = async (
+  subscriptionId: string,
+  customerId: string,
+  createAction = false,
+) => {
+  try {
+    const customerData = await db.query.customers.findFirst({
+      where: (c, { eq }) => eq(c.stripeCustomerId, customerId),
+    });
+    if (!customerData) throw new Error('🔴Cannot find the customer');
+    const { id: uuid } = customerData;
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
+      expand: ['default_payment_method'],
+    });
+    console.log('🟢UPDATED to  ', subscription.status);
+
+    const subscriptionData: Subscription = {
+      id: subscription.id,
+      userId: uuid,
+      metadata: subscription.metadata,
+      //@ts-ignore
+      status: subscription.status,
+      priceId: subscription.items.data[0].price.id,
+      //@ts-ignore
+      quantity: subscription.quantity,
+      cancelAtPeriodEnd: subscription.cancel_at_period_end,
+      cancelAt: subscription.cancel_at ? toDateTime(subscription.cancel_at).toISOString() : null,
+      canceledAt: subscription.canceled_at ? toDateTime(subscription.canceled_at).toISOString() : null,
+      currentPeriodStart: toDateTime(subscription.current_period_start).toISOString(),
+      currentPeriodEnd: toDateTime(subscription.current_period_end).toISOString(),
+      endedAt: subscription.ended_at ? toDateTime(subscription.ended_at).toISOString() : null,
+      trialStart: subscription.trial_start ? toDateTime(subscription.trial_start).toISOString() : null,
+      trialEnd: subscription.trial_end ? toDateTime(subscription.trial_end).toISOString() : null,
+    };
+    await db
+      .insert(subscriptions)
+      .values(subscriptionData)
+      .onConflictDoUpdate({ target: subscriptions.id, set: subscriptionData });
+    console.log(`Inserted/updated subscription [${subscription.id}] for user [${uuid}]`);
+    if (createAction && subscription.default_payment_method && uuid) {
+      await copyBillingDetailsToCustomer(uuid, subscription.default_payment_method as Stripe.PaymentMethod);
+    }
+  } catch (error) {
+    throw error;
   }
 };
