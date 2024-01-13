b9f154a8490a9b3e6f0403a523e2718dc1611e64
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-13 23:40:07 +0530

    feat(refactored) migration schemas , twconfig and auth layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [require('tailwind-scrollbar'), require('tailwind-scrollbar-hide')],
 };
 export default config;
