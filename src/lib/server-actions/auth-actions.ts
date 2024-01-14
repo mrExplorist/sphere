d3a643507846190a219cd6d817ec05cc4f8610e4
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-14 12:37:22 +0530

    fixed: small issue in authentication in production

--- src/lib/server-actions/auth-actions.ts
+++ src/lib/server-actions/auth-actions.ts
@@ -1,15 +1,12 @@
-"use server";
+'use server';
 
-import { z } from "zod";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { z } from 'zod';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
-import { cookies } from "next/headers";
-import { FormSchema } from "../types";
+import { cookies } from 'next/headers';
+import { FormSchema } from '../types';
 
-export async function actionLoginUser({
-  email,
-  password,
-}: z.infer<typeof FormSchema>) {
+export async function actionLoginUser({ email, password }: z.infer<typeof FormSchema>) {
   const supabase = createRouteHandlerClient({ cookies });
   const response = await supabase.auth.signInWithPassword({
     email,
@@ -18,22 +15,16 @@ export async function actionLoginUser({
   return response;
 }
 
-export async function actionSignUpUser({
-  email,
-  password,
-}: z.infer<typeof FormSchema>) {
+export async function actionSignUpUser({ email, password }: z.infer<typeof FormSchema>) {
   const supabase = createRouteHandlerClient({ cookies });
-  const { data } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("email", email);
+  const { data } = await supabase.from('profiles').select('*').eq('email', email);
 
-  if (data?.length) return { error: { message: "User already exists", data } };
+  if (data?.length) return { error: { message: 'User already exists', data } };
   const response = await supabase.auth.signUp({
     email,
     password,
     options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`,
     },
   });
   return response;
